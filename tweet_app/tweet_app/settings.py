4f4d1d07ca53ed41408789b81885a4b1e7a7f6cb
prahul25 <palrahulkumar2002@gmail.com>
2026-07-19 15:09:44 +0530

    feat: add Tailwind CSS, dark navbar, and live reload

--- tweet_app/tweet_app/settings.py
+++ tweet_app/tweet_app/settings.py
@@ -38,8 +38,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_browser_reload",
+    'tailwind',
+    'theme',
     "tweet",
 ]
+
+TAILWIND_APP_NAME = 'theme'
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -49,6 +54,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = "tweet_app.urls"
